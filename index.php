41b309c1b48e505207c3fb7f2f0d7602bc8d5c2b
mawah5035 <mariam_wahedi@icloud.com>
2025-09-10 12:22:02 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,1 @@
-<?php
-$navn = "Asia";
-echo "<p>Hei, $navn! Velkommen til nettsiden min.</p>";
-/*
-echo "Welcome to my home!";
-echo "Mi casa su casa!";
-*/
-echo "Hello!";
-$x = 5 /* + 15 */ + 5;
-echo $x;
-?>
+
