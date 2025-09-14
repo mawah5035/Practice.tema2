0ff50ffafee6e3310b39fc7ed6fa35134f3793bc
mawah5035 <mariam_wahedi@icloud.com>
2025-09-14 19:13:50 +0200

    Update and rename index.php to hilsen.php

---
+++ hilsen.php
@@ -0,0 +1,8 @@
+<?php
+// Tar imot data fra skjemaet
+$fornavn = $_POST["fornavn"];
+$etternavn = $_POST["etternavn"];
+
+// Skriver ut en hilsen
+echo "<h1>God dag $fornavn $etternavn!</h1>";
+?>
--- index.php
+++
@@ -1,6 +0,0 @@
-<?php    
- $fornavn=$_POST ["fornavn"];
-  $etternavn=$_POST ["etternavn"];  
-	
-  print ("God dag $fornavn $etternavn <br />");  
-?>
