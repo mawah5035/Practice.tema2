74131adb46361d8543481afb971f8d8412adf1f6
mawah5035 <mariam_wahedi@icloud.com>
2025-09-14 19:15:51 +0200

    Update hilsen.php

--- hilsen.php
+++ hilsen.php
@@ -1,7 +1,11 @@
 <?php
+// Slå på feilmeldinger for debugging
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 // Tar imot data fra skjemaet
-$fornavn = $_POST["fornavn"];
-$etternavn = $_POST["etternavn"];
+$fornavn = $_POST["fornavn"] ?? '';
+$etternavn = $_POST["etternavn"] ?? '';
 
 // Skriver ut en hilsen
 echo "<h1>God dag $fornavn $etternavn!</h1>";
